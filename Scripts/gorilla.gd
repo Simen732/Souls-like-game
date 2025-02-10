2b2cd6e068fff956bc5ca9216db5fb2897641359
Splur <sebastianstabaek@gmail.com>
2025-02-10 20:03:46 +0100

    Ga Robert walking animation og nytt angrep
    Eneste som mangler er rundt 3-4 flere angrep, death animation, og particles. Ellers er han ferdig

--- Scripts/gorilla.gd
+++ Scripts/gorilla.gd
@@ -2,30 +2,32 @@ extends CharacterBody3D
 
 @onready var animation_player = $AnimationPlayer
 @onready var music = $AudioStreamPlayer3D
-@onready var handL: Area3D = $Node/root/body/chest/armLeft/elbowLeft/handLeft/Area3D2
-@onready var handR: Area3D = $Node/root/body/chest/armRight/elbowRight/handRight/Area3D2
+@onready var handL: Area3D = $Node/root/body/chest/armLeft/elbowLeft/Area3D2
+@onready var handR: Area3D = $Node/root/body/chest/armRight/elbowRight/Area3D2
+@onready var body: Area3D = $Node/root/body/Area3D3
 @onready var boss_healthbar = $"../CharacterBody3D/BossHealthbar"
 @onready var hitbox = $Node/root/body/hitBox/GorillaHitbox
 @onready var hitArea = $Node/root/body/hitBox
 
 @export var rotatable = true
 @export var attackmove = 0
+@export var attackstop_distance = 2.5
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-var ranged_attack_timer = randi_range(60, 180)
+var jump_attack_timer = randi_range(480, 720)
 var Health = 150
 var aggro = false
 var speed = 3
-var attackstop_distance = 1
-var attacks = ["attack1", "attack2"]
+var attacks = ["attack2", "attack3"]
 var spawnpoint = Vector3(0, 0, 0)
 
-const aggro_range = 30
+const aggro_range = 45
 const attack23_range = 3  # Distance at which bro uses 1st 2nd and 4th attack
 const attack1_range = 1.5
 
 
 func _ready():
+	animation_player.play("idle")
 	spawnpoint = self.position
 	Global.restart.connect(on_restart)
 	Global.playerDealDamage.connect(on_playerDealDamage)
@@ -35,6 +37,9 @@ func _ready():
 		
 	if not handR.is_connected("area_entered", Callable(self, "_on_handR_area_entered")):
 		handR.connect("area_entered", Callable(self, "_on_handR_area_entered"))
+	
+	if not body.is_connected("area_entered", Callable(self, "_on_body_area_entered")):
+		body.connect("area_entered", Callable(self, "_on_body_area_entered"))
 
 
 func _physics_process(delta):
@@ -49,6 +54,7 @@ func _physics_process(delta):
 		boss_healthbar.value = boss_healthbar.max_value
 		boss_healthbar.visible = true
 		boss_healthbar.value = Health
+		animation_player.play("walk")
 
 	if aggro:
 		if boss_healthbar.value > Health:
@@ -58,7 +64,7 @@ func _physics_process(delta):
 		if typeof(Global.enemy_lock_on_position) == TYPE_VECTOR3:
 			move_towards_player(delta)
 		handle_attack(delta)
-		if self.global_position.distance_to(Global.player_position) > aggro_range or Health <= 1:
+		if self.global_position.distance_to(Global.player_position) > aggro_range:
 			aggro = false
 			Health = boss_healthbar.max_value
 			Global.isFighting = false
@@ -76,16 +82,18 @@ func on_restart():
 	boss_healthbar.visible = false
 	animation_player.stop()	
 	animation_player.play("idle")
+	jump_attack_timer = randi_range(480, 720)
 	self.position = spawnpoint
-	self.rotation = Vector3(0, 180, 0)
 
 
 
 func move_towards_player(delta):
 	var direction = Vector3(Global.enemy_lock_on_position.x - self.global_position.x, 0, Global.enemy_lock_on_position.z - self.global_position.z)
 	direction = direction.normalized()
-	if self.global_position.distance_to(Global.player_position) >= attackstop_distance:
+	if self.global_position.distance_to(Global.player_position) >= attackstop_distance and rotatable:
 		self.global_position += (speed + attackmove) * direction * delta
+	else:
+		global_translate(transform.basis.z * -attackmove * delta)
 	if rotatable:
 		var target_rotation_y = atan2(-direction.x, -direction.z)
 		self.rotation.y = lerp_angle(self.rotation.y, target_rotation_y, 0.05)
@@ -93,20 +101,30 @@ func move_towards_player(delta):
 
 # Handle attacking logic
 func handle_attack(delta):
-	var attackAnim = ["attack1", "attack2", "attack3"] #Array med liste av alle attacks
+	var attackAnim = ["attack1", "attack2", "attack3", "attack4"] #Array med liste av alle attacks
+	if self.global_position.distance_to(Global.player_position) > 7.5:
+		jump_attack_timer -= randi_range(3, 9)
+	else:
+		jump_attack_timer -= randi_range(1, 3)
 
-	if self.global_position.distance_to(Global.player_position) >= attack23_range:
-		ranged_attack_timer -= 1
+#Jump attack
+	if jump_attack_timer <= 0 and animation_player.current_animation not in attackAnim:
+		speed = 0
+		animation_player.play("attack4")
+		attackstop_distance = 0
+		await animation_player.animation_finished
+		speed = 3
+		attackstop_distance = 2.5
+		jump_attack_timer = randi_range(720, 1200)
+		animation_player.play("walk")
 
-#Attack1
-	elif self.global_position.distance_to(Global.player_position) <= attack1_range and animation_player.current_animation not in attackAnim:
+#Close range attack
+	if self.global_position.distance_to(Global.player_position) <= attack1_range and animation_player.current_animation not in attackAnim and randi_range(1, 3) > 1:
 		speed = 0
 		animation_player.play("attack1")
 		await animation_player.animation_finished
 		speed = 3
-		attackstop_distance = 0
-		ranged_attack_timer = randi_range(60, 180)
-		animation_player.play("idle")
+		animation_player.play("walk")
 
 #Mid range attacks
 	if self.global_position.distance_to(Global.player_position) <= attack23_range and animation_player.current_animation not in attackAnim: #sjekker om den er i midten av et angrep
@@ -118,7 +136,7 @@ func handle_attack(delta):
 		print(attacks)
 		await animation_player.animation_finished
 		speed = 3
-		animation_player.play("idle")
+		animation_player.play("walk")
 
 
 # Handle when the sword hits an area
@@ -129,6 +147,10 @@ func _on_handL_area_entered(area: Area3D) -> void:
 func _on_handR_area_entered(area: Area3D) -> void:
 	if area.name == "Player" or area.get_parent().name == "Player" and !handR.disabled:
 		Global.playerTakeDamage.emit(40)
+
+func _on_body_area_entered(area: Area3D) -> void:
+	if area.name == "Player" or area.get_parent().name == "Player" and !body.disabled:
+		Global.playerTakeDamage.emit(60)
 
 
 func on_playerDealDamage(area):
